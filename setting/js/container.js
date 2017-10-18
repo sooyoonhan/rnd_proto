daa64d0178fcae587c0d5d3849e8b20bf766e318
arie4208 <arie4208@gmail.com>
2017-10-18 11:46:29 +0900

    메뉴갱신, 메뉴렌더링
    1. MenuData.json 내 메뉴 및 경로 갱신
    2. container.js 메뉴 callback데이터 연동 및 렌더링 갱신

--- setting/js/container.js
+++ setting/js/container.js
@@ -1,33 +1,12 @@
 //로더
 var loader = $("#loader");
-loader.hide();
 
 //언어셀렉트
 var language = $("#language");
 
-////언어팩로드
-language.val(sessionStorage.getItem('langType') !== null ? sessionStorage.getItem('langType') : 'ko');
-//getLang('menuname', function(lang, error){
-//	//세션내용
-//	console.log(sessionStorage);
-//	
-//	//에러체크
-//	if(error){
-//		alert(error);
-//
-//		//로더비활성화
-//		loader.hide();
-//		return false;
-//	}
-//
-//	//언어팩로드완료
-//	console.log(lang);
-//
-//	//로더비활성화
-//	loader.hide();
-//}, true);
-
 //언어팩변경
+console.log(sessionStorage)
+language.val(sessionStorage.getItem('langType') !== null ? sessionStorage.getItem('langType') : 'ko');
 language.on("change", function(e){
 	e.preventDefault();
 
@@ -35,87 +14,140 @@ language.on("change", function(e){
 	location.reload();
 });
 
-//전체 메뉴 리스트 가져오기
+//전체메뉴리스트가져오기
 getMenu(function(lang, error) {
-
+	//에러체크
 	if(error) {
 		alert(error);
+
+		//로더비활성화
+		loader.hide();
 		return false;
 	}
 
-//	alert(lang);
+	//전체메뉴리스트가져오기완료
+	//console.log(lang);
 
-}, true);
+	//메뉴렌더링
+	lenderMenu(lang);
+}, false);
+
+//메뉴렌더링
+function lenderMenu(getMenuCallback){
+	console.log(getMenuCallback);
+	
+	//네비게이션컨테이너
+	var container = $("nav > ul");
+
+	var str = '';
+	var bigMenu = getMenuCallback["menu"];
+	console.log(bigMenu[1])
+	for(var i=0; i<bigMenu.length; i++){
+		str += '<li>';
+			//대메뉴렌더링
+			str += '<a href="' + bigMenu[i]["url"] + '">';
+				str += '<span>' + bigMenu[i]["menuName"] + '</span>';
+				str += '<i class="fa fa-angle-down" aria-hidden="true"></i>';
+			str += '</a>';
+			//서브메뉴렌더링
+			var subMenu = bigMenu[i]["subMenu"];
+			if(subMenu != undefined){
+				//서브메뉴있음
+				str += '<ul>';
+					for(var j=0; j<subMenu.length; j++){
+						str += '<li>';
+							str += '<a href="' + subMenu[j]["url"] + '" target="contents">';
+								str += '<i class="fa fa-caret-right" aria-hidden="true"></i> ';
+								str += '<span>' + subMenu[j]["menuName"] + '</span>';
+							str += '</a>';
+						str += '</li>';
+					}
+				str += '</ul>';
+			}
+		str += '<li>';
+	}
+
+	//렌더링
+	container.append(str);
+
+	//로더비활성화
+	loader.hide();
+
+	//버튼갱신
+	btnsRefesh();
+}
+
+//버튼갱신
+function btnsRefesh(){
+	//네비게이션
+	var bMenu = $("nav > ul > li > a");		//대메뉴
+	var sMenuCon = $("nav > ul > li ul");	//소메뉴컨테이너
+	var sMenu = $("nav > ul > li ul a");	//소메뉴
+	var container = $("[name=contents]");	//화면표시Iframe
+	var navigation = $("#navigation");		//네비게이션
+	//대메뉴클릭
+	bMenu.unbind("click").bind("click", function(e){
+		e.preventDefault();
+
+		//클래스핸들링
+		bMenu.parent().removeClass("ov");
+		sMenu.removeClass("ov");
+		$(this).parent().addClass("ov");
+
+		//부모텍스트
+		var parentText = $(this).find("span").text();
 
 
-//네비게이션
-var bMenu = $("nav > ul > li > a");		//대메뉴
-var sMenuCon = $("nav > ul > li ul");	//소메뉴컨테이너
-var sMenu = $("nav > ul > li ul a");	//소메뉴
-var container = $("[name=contents]");	//화면표시Iframe
-var navigation = $("#navigation");		//네비게이션
-//대메뉴클릭
-bMenu.on("click", function(e){
-	e.preventDefault();
+		//하위메뉴유무에따른작동
+		var isBool = $(this).next().is("ul");
+		if(isBool){
+			//하위메뉴있음
+			var target = $(this).next().find("a").eq(0);
+			target.addClass("ov");
+			container.attr("src", target.attr("href"));
 
-	//클래스핸들링
-	bMenu.parent().removeClass("ov");
-	sMenu.removeClass("ov");
-	$(this).parent().addClass("ov");
+			//자식텍스트
+			var subText = target.find("span").text();
 
-	//부모텍스트
-	var parentText = $(this).find("span").text();
+			//네비게이션갱신
+			var str = 'MAIN ';
+			str += '<i class="fa fa-angle-right" aria-hidden="true"></i> ' + parentText;
+			str += '<i class="fa fa-angle-right" aria-hidden="true"></i> ' + subText;
+			navigation.html(str);
 
+		} else{
+			//하위메뉴없음
+			container.attr("src", $(this).attr("href"));
 
-	//하위메뉴유무에따른작동
-	var isBool = $(this).next().is("ul");
-	if(isBool){
-		//하위메뉴있음
-		var target = $(this).next().find("a").eq(0);
-		target.addClass("ov");
-		container.attr("src", target.attr("href"));
-
-		//자식텍스트
-		var subText = target.find("span").text();
+			//네비게이션갱신
+			var str = 'MAIN ';
+			str += '<i class="fa fa-angle-right" aria-hidden="true"></i> ' + parentText;
+			navigation.html(str);
+		}
+	});
+	//소메뉴클릭작동
+	sMenu.unbind("click").bind("click", function(e){
+		//클래스핸들링
+		sMenu.removeClass("ov");
+		$(this).addClass("ov");
 
 		//네비게이션갱신
+		//부모텍스트
+		var parentText = $(this).parent().parent().parent().find("span").eq(0).text();
+		//자식텍스트
+		var subText = $(this).find("span").text();
 		var str = 'MAIN ';
 		str += '<i class="fa fa-angle-right" aria-hidden="true"></i> ' + parentText;
 		str += '<i class="fa fa-angle-right" aria-hidden="true"></i> ' + subText;
 		navigation.html(str);
-
-	} else{
-		//하위메뉴없음
-		container.attr("src", $(this).attr("href"));
-
-		//네비게이션갱신
-		var str = 'MAIN ';
-		str += '<i class="fa fa-angle-right" aria-hidden="true"></i> ' + parentText;
-		navigation.html(str);
-	}
-});
-//소메뉴클릭작동
-sMenu.on("click", function(e){
-	//클래스핸들링
-	sMenu.removeClass("ov");
-	$(this).addClass("ov");
-
-	//네비게이션갱신
-	//부모텍스트
-	var parentText = $(this).parent().parent().parent().find("span").eq(0).text();
-	//자식텍스트
-	var subText = $(this).find("span").text();
-	var str = 'MAIN ';
-	str += '<i class="fa fa-angle-right" aria-hidden="true"></i> ' + parentText;
-	str += '<i class="fa fa-angle-right" aria-hidden="true"></i> ' + subText;
-	navigation.html(str);
-});
+	});
 
 
-//로그인버튼클릭
-var loginBtn = $("#loginBtn");
-loginBtn.on("click", function(e){
-	e.preventDefault();
+	//로그인버튼클릭
+	var loginBtn = $("#loginBtn");
+	loginBtn.unbind("click").bind("click", function(e){
+		e.preventDefault();
 
-	location.href = "body.html";
-});
+		location.href = "body.html";
+	});
+}
