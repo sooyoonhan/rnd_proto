c115d1fa9bc8955e9da875058742cec3f9fa07c8
darkdru1989 <darkdru1989@gmail.com>
2017-10-18 12:46:22 +0900

    depth별 메뉴 리스트 가져오기, 전체 메뉴 언어 리스트 가져오기 추가

--- setting/js/menuData.js
+++ setting/js/menuData.js
@@ -6,25 +6,18 @@
 	
 */
 
-function getMenu(callBack,isJson)
-{
+//화면에 보여질 메뉴 정보 가져오기
+function getMenu(callBack,isJson) {
 	isJson = typeof isJson !== 'undefined' ? isJson : true; //디폴트값 설정
 	
-	var path = "setting/lang/";
 	
-	path += sessionStorage.getItem('langType') !== null ? sessionStorage.getItem('langType') : 'ko';
-	
-	var langName = path + "/common/menuData.json"; //최종 호출 경로 설정
-	
-	$.getJSON(langName) //언어파일 불러오기
-		.done(function(result) { //성공
-			if(result === null)
-			{
-				callBack(null,'not Lang');
-			}
-			
-			//권한에 맵핑된 메뉴 데이터 가져오기 
-			getAuthMenu(1, function(data,error) {
+	getMenuJson(function(result,error) {
+		if(error) {
+			callBack(null,error);
+			return false;
+		}
+
+		getAuthMenu(1, function(data,error) {
 				if(error)
 				{
 					alert(error);
@@ -44,13 +37,96 @@ function getMenu(callBack,isJson)
 				}
 
 			});
-			
-		})
-		.fail(function(jqxhr,textStatus,error) { //실패
-			var err = "menuData "+' '+error ;
-			callBack(null,err);
-		});
+
+	});
+
+}
+
+//특정 depth의 메뉴 언어 리스트 가져오기
+function getDepthMenuList(depth,callBack,isJson) {
+	isJson = typeof isJson !== 'undefined' ? isJson : true; //디폴트값 설정
+
+	getMenuJson(function(result,error) {
+		if(error) {
+			callBack(null,error);
+			return false;
+		}
+
+		getDepthMenu(depth, function(data,error) {
+				if(error)
+				{
+					alert(error);
+					return false;
+				}
+
+				if(data)
+				{
+					mappingMenuLang(data,result,function(menuData) {
+
+						if(isJson)
+							callBack(JSON.stringify(menuData),null);
+						else
+							callBack(menuData,null);
+
+					});
+				}
+
+			});
+
+	});
+}
+
+//메뉴 언어 리스트 가져오기 
+function getMenuLangList(callBack,isJson) {
+	isJson = typeof isJson !== 'undefined' ? isJson : true; //디폴트값 설정
 	
+	
+	getMenuJson(function(result,error) {
+		if(error) {
+			callBack(null,error);
+			return false;
+		}
+
+	
+		if(isJson)
+			callBack(JSON.stringify(result),null);
+		else
+			callBack(result,null);
+
+				
+	});
+
+}
+
+//지정한 depth의 메뉴 리스트 가져오기 
+function getDepthMenu(depth_val, callBack) {
+	
+	const jsonArg = { param:{ depth:depth_val } };
+	
+	var param = JSON.stringify(jsonArg);
+	
+	$.ajax({
+		url:"http://106.241.53.172:3000/adminSetting/menu/get",
+		contentType:"application/json",
+		//datatype:"json",
+		data:param,
+		method:"POST",
+		error: function(){
+			   alert('Error loading XML document');
+			},
+		success:function(result){
+			try {
+
+					callBack(result);
+					
+			}
+			catch(e) {
+
+				callBack(null,e);
+
+			}
+		}
+	});
 }
 
 //권한별 메뉴정보 가져오기 
@@ -89,6 +165,11 @@ function mappingMenuData(authMenu, langMenuData, callBack) {
 	var mainMenu = {};
 	
 	const result = JSON.parse(authMenu);
+
+	if(result == null) {
+		callBack(null);
+		return;
+	}
 
 	//대메뉴 추출
 	mainMenu.menu = result.data.filter(function(value) {
@@ -147,4 +228,54 @@ function mappingMenuData(authMenu, langMenuData, callBack) {
 	//console.log(JSON.stringify(MenuData.menu));
 
 	callBack(MenuData);
+}
+
+//메뉴데이터와 메뉴 언어 데이터 맵핑
+function mappingMenuLang(menu, langMenu, callBack) {
+	var menuData = [];
+	
+	const result = JSON.parse(menu);
+
+	if(result == null) {
+		callBack(null);
+		return;
+	}
+
+	result.data.forEach(function(val,i) {
+
+		const menuLang = langMenu.menu.filter(function(value) {
+				return value.seq === val.MENU_SEQ;
+		});
+		
+		if(menuLang.length)
+			menuData.push(menuLang[0]);
+
+	});
+
+	callBack(menuData);
+}
+
+//menuData.json에서 메뉴 데이터 가져오기
+function getMenuJson(callBack)
+{
+	var path = "setting/lang/";
+	
+	path += sessionStorage.getItem('langType') !== null ? sessionStorage.getItem('langType') : 'ko';
+	
+	var langName = path + "/common/menuData.json"; //최종 호출 경로 설정
+	
+	$.getJSON(langName) //언어파일 불러오기
+		.done(function(result) { //성공
+			if(result === null)
+			{
+				callBack(null,'not Lang');
+				return;
+			}
+			
+				callBack(result,null);
+		})
+		.fail(function(jqxhr,textStatus,error) { //실패
+			var err = "menuData "+' '+error ;
+			callBack(null,err);
+		});
 }
